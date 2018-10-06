0cacdc6c2b7704c97dd962bc2e0e53219dddd1ed
root5533 <ymuraad@gmail.com>
2018-10-06 21:02:16 +0530

    Change sample images

--- public/frontend/src/app/constants/imageFiles.ts
+++ public/frontend/src/app/constants/imageFiles.ts
@@ -2,8 +2,8 @@ export const IMAGES = [
     { id: 1, name: 'Image 1', main: 'assets/images/image1.jpg', thumbnail: 'assets/images/image1.jpg' },
     { id: 1, name: 'Image 2', main: 'assets/images/image2.jpg', thumbnail: 'assets/images/image2.jpg' },
     { id: 1, name: 'Image 3', main: 'assets/images/image3.jpg', thumbnail: 'assets/images/image3.jpg' },
-    { id: 1, name: 'Image 4', main: 'assets/images/image1.jpg', thumbnail: 'assets/images/image1.jpg' },
-    { id: 1, name: 'Image 5', main: 'assets/images/image2.jpg', thumbnail: 'assets/images/image2.jpg' },
-    { id: 1, name: 'Image 6', main: 'assets/images/image3.jpg', thumbnail: 'assets/images/image3.jpg' },
+    { id: 1, name: 'Image 4', main: 'assets/images/image4.jpg', thumbnail: 'assets/images/image4.jpg' },
+    { id: 1, name: 'Image 5', main: 'assets/images/image1.jpg', thumbnail: 'assets/images/image1.jpg' },
+    { id: 1, name: 'Image 6', main: 'assets/images/image2.jpg', thumbnail: 'assets/images/image2.jpg' },
     { id: 1, name: 'Image 7', main: 'assets/images/angular.png', thumbnail: 'assets/images/angular.png' },
 ];
