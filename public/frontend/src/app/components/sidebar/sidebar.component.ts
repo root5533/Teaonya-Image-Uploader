156a7d12869714c6ff28c96a9674e7e25d65cec8
root5533 <ymuraad@gmail.com>
2018-10-13 18:11:49 +0530

    Load tags and recent images on upload

--- public/frontend/src/app/components/sidebar/sidebar.component.ts
+++ public/frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,6 +13,7 @@ export class SidebarComponent implements OnInit {
   mobile = false;
   sidebarToggle = true;
   tags: any;
+  _selectedTag: Observable<any>;
 
   private _toggleSidebar() {
     this._opened = !this._opened;
@@ -20,10 +22,18 @@ export class SidebarComponent implements OnInit {
   constructor( public sidebarService: SidebarService ) { }
 
   ngOnInit() {
+    this._selectedTag = this.sidebarService.getImagesObservable;
     this.sidebarService.getTags().subscribe((tags) => {
       if (tags) {
         this.tags = tags['tags'];
         // console.log(this.tags);
+      }
+    });
+    this._selectedTag.subscribe((tag) => {
+      if (tag === 'Home') {
+        this.sidebarService.getTags().subscribe((tags) => {
+          this.tags = tags['tags'];
+        });
       }
     });
   }
